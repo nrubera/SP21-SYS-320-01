1f3598c15a44a05a047ed2de25d61583ec1f1488
unknown <nicholas.rubera@CYBER.LOCAL>
2021-03-16 15:13:37 -0400

    Complete Week 8 Scripts

---
+++ Week08/homework/EmailScript.ps1
@@ -0,0 +1,13 @@
+﻿ # Stroyline: Send an email
+
+ # Body of the email
+ $msg = "Hello There."
+
+ # Email from address
+ $email = "nicholas.rubera@mymail.champlain.edu"
+
+ # To address
+ $toEmail = "deployer@csi-web"
+
+ # Send the email
+ Send-MailMessage -From $email -to $toEmail -Subject "A Greeting" -body $msg -SmtpServer 192.168.6.71
---
+++ Week08/homework/EventLogScript.ps1
@@ -0,0 +1,14 @@
+﻿# Storyline: Review the Security Event Log
+
+# List all the avaliable Windows Event Logs
+Get-EventLog -List
+
+# Create a prompt to allow user to select log to view
+$readLog = Read-Host -Prompt "Please select a log to review from the list above:"
+
+# Creates a prompt to allow user to enter a search phrase
+$readPhrase = Read-Host -Prompt "Please enter search phrase for log:"
+
+#Print the log (Video 2 challenge)
+Get-EventLog -LogName $readLog -Newest 40 | where {$_.Message -ilike $readPhrase } | export-csv -NoTypeInformation `
+-Path "C:\Users\nicholas.rubera\Desktop\logs.csv"
