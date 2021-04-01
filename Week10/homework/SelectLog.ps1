b837dec4fed55704027068239da5db1ef59b6618
unknown <nicholas.rubera@cyber.local>
2021-04-01 12:04:15 -0400

    Completed Week 10 Script based on feedback

---
+++ Week10/homework/SelectLog.ps1
@@ -0,0 +1,80 @@
+﻿# Storyline: View the event logs, check for a valid log, and print the results
+
+function select_log() {
+    
+    cls
+    # List all event logs
+    $theLogs = Get-EventLog -list | select Log
+    $theLogs | Out-Host
+
+    # Initialize the array to stroe the logs
+    $arrLog = @()
+
+    foreach ($tempLog in $theLogs) {
+        
+        # Add each log to the array
+        # NOTE: These are stored in the array as a hastable in the format:
+        # @{Log=LOGNAME}
+        $arrLog += $tempLog
+    }
+
+    # Prompt the user for the log to view or quit
+    $readLog = read-host -Prompt "Please enter a log from the list above or 'q' to quit the program"
+
+    # Check if the user wants to quit
+    if ($readLog -match "^[qQ]$"){
+
+        break
+    }
+
+    log_check -logToSearch $readLog
+}
+
+
+function log_check() {
+
+    # String user types in within the select_log function
+    Param([string]$logToSearch)
+
+    # Format the user's input
+    $theLog = "^@[Log=" + $logToSearch + "]$"
+
+    # Search the array for the exact hashtable string
+    if ($arrLog -match $theLog){
+
+        write-host -BackgroundColor Green -ForegroundColor White "Please wait, it may take a few moments to get the log entries"
+        sleep 2
+
+        # Call the function to view the log
+        view_log -logToSearch $logToSearch
+
+    } else {
+
+        write-host -BackgroundColor red -ForegroundColor White "The log does not exist"
+        sleep 2
+
+        select_log
+
+    }
+
+
+}
+
+
+function view_log(){
+
+    cls
+
+    # Get the logs
+    Get-EventLog -Log $logToSearch -Newest 10 -after "3/31/2021"
+
+    # Pause the screen adn wait until the user is ready to proceed
+    read-host -Prompt "Press enter when you are done"
+
+    # Go back to select_log
+    select_log
+
+}
+
+# Run the select_log function first
+select_log
---
+++ Week10/homework/ServiceScript.ps1
@@ -0,0 +1,47 @@
+﻿# Stroyline: List all services running, stopped, or all services
+
+function get_service() {
+$arrService =@('All', 'Stopped', 'Running')
+
+$readService = Read-Host -Prompt "Please enter a service state (Running, Stopped, All) or 'q' to quit"
+
+if ($readService -match "^[qQ]$"){ # Quits program
+    
+      break
+}
+
+if ($readService -match "Running"){ # Prints only running services
+
+     Get-WmiObject win32_service |  where {$_.State -eq $arrService[2]
+} | Select Name, DisplayName, StartMode | sort Name
+
+}
+
+if ($readService -match "Stopped"){ # Prints only stopped services
+
+     Get-WmiObject win32_service |  where {$_.State -eq $arrService[1]
+} | Select Name, DisplayName, StartMode | sort Name
+
+
+}
+
+
+if ($readService -match "All"){ # Prints All services
+
+     Get-WmiObject win32_service | Select Name, DisplayName, StartMode | sort Name
+
+}
+
+if ($readService -notmatch "Running","Stopped","All","q","Q"){
+
+    write-host -BackgroundColor red -ForegroundColor White "Please enter as valid state."
+       sleep 2
+       get_service
+
+}
+
+} # end function
+
+get_service
+
+
